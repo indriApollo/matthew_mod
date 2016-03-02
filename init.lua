8b04aaaf1aa2c2d87c350b78363b067001181d25
raphael leroux <indri.apollo@gmail.com>
2016-03-02 15:05:40 +0100

    update

--- init.lua
+++ init.lua
@@ -5,6 +5,10 @@ matthew_mod.http = minetest.request_http_api()
 if not matthew_mod.http then
 	minetest.log("error","Http accesss not granted")
 else
-	dofile( minetest.get_modpath("matthew_mod").."/mod.lua" )
+	local modpath = minetest.get_modpath("matthew_mod")
+
+	dofile( modpath.."/base64.lua" )
+	dofile( modpath.."/mod.lua" )
+	
 	minetest.log("info","matthew_mod is up and running")
 end
--- mod.lua
+++ mod.lua
@@ -1,5 +1,5 @@
 --matthew_mod
-
+matthew_mod.list = {}
 --block
 minetest.register_node("matthew_mod:play", {
 	description = "matthew_mod play button",
@@ -24,8 +24,48 @@ minetest.register_node("matthew_mod:pause", {
 --functions
 local http = matthew_mod.http
 
+matthew_mod.loadList = function()
+
+	minetest.log("action","loadlist")
+	
+	local req = {url="http://localhost:8888/list",
+				timeout=10,
+				post_data = nil, --use GET
+				user_agent="minetest http"}
+	http.fetch(req, matthew_mod.loadList_callback)
+end
+
+matthew_mod.loadList_callback = function(resp)
+
+	if resp.code ~= 200 then
+		minetest.log("error","Could not load music list !(http "..resp.code..")")
+	else
+		local data = minetest.parse_json(resp.data)
+		local list = data.message
+		for i,v in ipairs(list) do
+			matthew_mod.list[i] = {}
+			matthew_mod.list[i].name = v.name
+			matthew_mod.list[i].m_id = matthew_mod.base64enc(v.name)
+		end
+		minetest.log("action",#list.." songs loaded in music list")
+	end
+end
+
 matthew_mod.play = function()
-	--
+	math.randomseed( os.time() )
+	if matthew_mod.list[1] then
+		local n = math.random(#matthew_mod.list)
+		local m_id = matthew_mod.list[n].m_id
+		local req = {url="http://localhost:8888/play/"..m_id,
+				timeout=10,
+				post_data = nil, --use GET
+				user_agent="minetest http"}
+	http.fetch(req, matthew_mod.play_callback)
+	end
+end
+
+matthew_mod.play_callback = function (resp)
+	minetest.chat_send_all(resp.data)
 end
 
 matthew_mod.pause = function()
@@ -40,3 +80,6 @@ end
 matthew_mod.pause_callback = function (resp)
 	minetest.chat_send_all(resp.data)
 end
+
+--onload
+minetest.after(0, matthew_mod.loadList )
